c0f46d00a83ce4c0a64625d15f9c58ec4fa3e369
Gabriel Santos <gabrieljorgesilvasantos@outlook.com>
2024-10-24 21:40:19 -0300

    nuxt 3 course

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h2>About</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas, expedita!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, maxime.</p>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <p>Product details for {{ id }}</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, sunt.</p>
+    </div>
+</template>
+
+<script setup>
+const { id } = useRoute().árams
+</script>
+
+<style lang="scss" scoped></style>
